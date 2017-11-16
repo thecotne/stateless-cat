04b493e3867be642c7d60582ec2e41a1429a436b
cotne nazarashvili <cotne.95@gmail.com>
2017-11-16 21:19:37 +0400

    add bootstrap

--- src/js/modules/petstore/views/IndexPage.js
+++ src/js/modules/petstore/views/IndexPage.js
@@ -3,6 +3,14 @@ import React from 'react'
 
 export default () =>
   <div>
-    <p>Hello World!</p>
-    <Link to='/petstore'>go to InnerPage</Link>
+    <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
+      <div className='container'>
+        <a className='navbar-brand' href='javascript:'>Stateless Cat</a>
+      </div>
+    </nav>
+
+    <main role='main' className='container'>
+      <p>Hello World!</p>
+      <Link to='/petstore'>go to InnerPage</Link>
+    </main>
   </div>
--- src/js/modules/petstore/views/InnerPage.js
+++ src/js/modules/petstore/views/InnerPage.js
@@ -3,6 +3,14 @@ import React from 'react'
 
 export default () =>
   <div>
-    <p>InnerPage Hello World!</p>
-    <Link to='/'>go to IndexPage</Link>
+    <nav className='navbar navbar-expand-md navbar-dark bg-dark fixed-top'>
+      <div className='container'>
+        <a className='navbar-brand' href='javascript:'>Stateless Cat</a>
+      </div>
+    </nav>
+
+    <main role='main' className='container'>
+      <p>InnerPage Hello World!</p>
+      <Link to='/'>go to IndexPage</Link>
+    </main>
   </div>
